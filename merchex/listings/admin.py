c2d869bf7a46c6665913450be687bff9df24f091
Seungto <assel.stephane@gmail.com>
2023-08-01 22:35:19 +0200

    partie 3

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from listings.models import Band
+from listings.models import Liste
+
+class BandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year_formed', 'genre', 'active')
+admin.site.register(Band, BandAdmin)
+
+class ListeAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'sold', 'type', 'band')
+admin.site.register( Liste, ListeAdmin)
--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,6 +1,35 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Band(models.Model):
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(max_length=50)
+    biography = models.fields.CharField(max_length=100)
+    year_formed = models.fields.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2021)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null= True, blank=True)
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    def __str__(self):
+        return f'{self.name}'
+
 class Liste(models.Model):
     titre = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=100)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(validators=[MinValueValidator(1950), MaxValueValidator(2023)])
+    type = models.fields.CharField(max_length=50)
+    class Type(models.TextChoices):
+        Records = 'REC'
+        Clothing = 'CLO'
+        Posters = 'POS'
+        Miscellaneous = 'MIS'
+    type = models.fields.CharField(choices=Type.choices, max_length=5)
+    def __str__(self):
+        return f'{self.titre}'
+    band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
